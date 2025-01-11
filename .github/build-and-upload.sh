b3353c1154a4d7a0ac160ff7923b303f7c8c2203
readapt io <48756323+readaptio@users.noreply.github.com>
2025-01-11 17:16:58 +0100

    Update build-and-upload.sh

--- .github/build-and-upload.sh
+++ .github/build-and-upload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Buidling and pushing israelhikingmap/graphhopper:latest"
+echo "Buidling and pushing readapt/graphhopper:latest"
 ./build.sh --push
 
 TAG=`cd graphhopper; git for-each-ref --sort=committerdate refs/tags | sed -n '$s/.*\///p'`
-if docker manifest inspect "israelhikingmap/graphhopper:${TAG}" >/dev/null; then 
+if docker manifest inspect "readaptio/graphhopper:${TAG}" >/dev/null; then 
     echo "No need to push existing version: ${TAG}";
 else
-    echo "Buidling and pushing israelhikingmap/graphhopper:${TAG}"
+    echo "Buidling and pushing readaptio/graphhopper:${TAG}"
     ./build.sh --push "${TAG}"
 fi
